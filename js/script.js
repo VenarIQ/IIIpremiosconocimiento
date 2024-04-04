9725d4de7737f4f30668c86b46cbbcded2c59c2a
VenarIQ <165713505+VenarIQ@users.noreply.github.com>
2024-04-04 11:02:38 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,386 @@
+
+  // QUESTIONS
+
+const questions = [
+  {
+    "question": "Se desenvuelve en situaciones cotidianas con una soltura inesperada y admirable",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3",
+  },
+  {
+    "question": "Tiene sentido del humor",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Demuestra un espíritu observador agudo y despierto",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Es atenta , detallista y exquisito en el trato",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Considera las situaciones problemáticas como un reto",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Demuestra curiosidad por saber de todo, incluso lo que no se da en clase",
+    "answer1":  "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Es muy sensible ante las injusticias, los fracasos y la incomprensión",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Es persistente y perfeccionista en las tareas que emprende",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Sus intereses son múltiples y variados",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Entre sus compañeros sobresale porque comprende las explicaciones y las asimila con rapidez",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Prefiere relacionarse con mayores",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Con frecuencia vive absorta en su mundo interior como distraída",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Sobresale por su disponibilidad y responsabilidad en las tareas de grupo",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Su comprensión es global. Diferencia con facilidad lo principal de lo secundario",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Resuelve con rapidez y acierto problemas",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Utiliza y organiza múltiples estrategias para estudiar y aprender",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Se organiza de forma que saca tiempo para todo",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Su vocabulario y fluidez verbal son ricos y elaborados para su edad",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Le apasiona la lectura, devora cuentos y libros",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Sus preguntas son incisivas",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Tiene una imaginación desbordante y creativa",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Se aburre y muestra desgana en clases rutinarias",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Es muy madura para su edad",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Si esta concentrada le molesta que le interrumpan",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Transfiere con facilidad los conocimientos y las estrategias aprendidos a otros contenidos y situaciones",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Capta con perspicacia las motivaciones de la gente, sus puntos débiles, sus necesidades y comprende sus problemas",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Reproduce con exactitud los contenidos aprendidos",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Posee amplia información sobre ciertos asuntos que no son propios de su edad",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "No suele satisfacerle el resultado o el ritmo de su trabajo. Siempre está segura de que puede y debería mejorarlo",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  },
+  {
+    "question": "Recibe poca ayuda del profesor en sus tareas. Es autosuficiente y autodidacta ",
+    "answer1": "1",
+    "answer1Total": "1",
+    "answer2": "2",
+    "answer2Total": "2",
+    "answer3": "3",
+    "answer3Total": "3"
+  }
+  
+  
+
+]
+
+
+let currentQuestion = 0;
+let score = [];
+let selectedAnswersData = [];
+const totalQuestions =questions.length;
+
+const container = document.querySelector('.quiz-container');
+const questionEl = document.querySelector('.question');
+const option1 = document.querySelector('.option1');
+const option2 = document.querySelector('.option2');
+const option3 = document.querySelector('.option3');
+const nextButton = document.querySelector('.next');
+const previousButton = document.querySelector('.previous');
+const restartButton = document.querySelector('.restart');
+const result = document.querySelector('.result');
+
+//Function to generate question 
+function generateQuestions (index) {
+    //Select each question by passing it a particular index
+    const question = questions[index];
+    const option1Total = questions[index].answer1Total;
+    const option2Total = questions[index].answer2Total;
+    const option3Total = questions[index].answer3Total;
+    //Populate html elements 
+    questionEl.innerHTML = `${index + 1}. ${question.question}`
+    option1.setAttribute('data-total', `${option1Total}`);
+    option2.setAttribute('data-total', `${option2Total}`);
+    option3.setAttribute('data-total', `${option3Total}`);
+    option1.innerHTML = `${question.answer1}`
+    option2.innerHTML = `${question.answer2}`
+    option3.innerHTML = `${question.answer3}`
+}
+
+
+function loadNextQuestion () {
+    const selectedOption = document.querySelector('input[type="radio"]:checked');
+    //Check if there is a radio input checked
+    if(!selectedOption) {
+        alert('Por favor, selecciona una respuesta');
+        return;
+    }
+    //Get value of selected radio
+    const answerScore = Number(selectedOption.nextElementSibling.getAttribute('data-total'));
+
+    ////Add the answer score to the score array
+    score.push(answerScore);
+
+    selectedAnswersData.push()
+    
+
+    const totalScore = score.reduce((total, currentNum) => total + currentNum);
+
+    //Finally we incement the current question number ( to be used as the index for each array)
+    currentQuestion++;
+
+        //once finished clear checked
+        selectedOption.checked = false;
+    //If quiz is on the final question
+    if(currentQuestion == totalQuestions - 1) {
+        nextButton.textContent = 'Terminar';
+    }
+    //If the quiz is finished then we hide the questions container and show the results 
+    if(currentQuestion == totalQuestions) {
+        container.style.display = 'none';
+        result.style.display = 'flex';
+        result.innerHTML =
+         `<h1 class="final-score">Puntos: ${totalScore}</h1>
+         <div class="summary">
+            <h1>Sumario</h1>
+            <p>Si supera los 70 puntos</p>
+            <p>sería conveniente contactar con un especialista</p>
+        </div>
+        <button class="restart">Reiniciar</button>
+         `;
+        return;
+    }
+    generateQuestions(currentQuestion);
+}
+
+//Function to load previous question
+function loadPreviousQuestion() {
+    //Decrement quentions index
+    currentQuestion--;
+    //remove last array value;
+    score.pop();
+    //Generate the question
+    generateQuestions(currentQuestion);
+}
+
+//Fuction to reset and restart the quiz;
+function restartQuiz(e) {
+    if(e.target.matches('button')) {
+    //reset array index and score
+    currentQuestion = 0;
+    score = [];
+    //Reload quiz to the start
+    location.reload();
+    }
+
+}
+
+
+generateQuestions(currentQuestion);
+nextButton.addEventListener('click', loadNextQuestion);
+previousButton.addEventListener('click',loadPreviousQuestion);
+result.addEventListener('click',restartQuiz);
+
